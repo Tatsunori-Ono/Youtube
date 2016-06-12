4b1364d48a8ca9137a62e07bfdb9790a7433cb5b
Tatsunori Ono <tatsunori.no1@gmail.com>
2016-06-12 16:39:43 +0900

    First commit

---
+++ YouTube/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YouTube
+//
+//  Created by Tatsunori Ono on 2016/06/12.
+//  Copyright © 2016年 Tatsunori Ono. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
